32e8c2db2acc9422257a4e5c42a6848990b94d75
Fsociety-Mrn <lisboamillen30@gmail.com>
2022-10-17 18:26:12 +0800

    add a TDS Level

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,52 +1,255 @@
-import { Button, Grid } from '@mui/material'
+import { Grid, Paper, Switch, Typography } from '@mui/material'
 import { Stack } from '@mui/system'
 import React from 'react'
-import HelloFriend from '../images/HelloFriend.png'
 
-
-import { LogoutHydro } from '../Authentication/LoginFirebase'
+// icons
+import AirIcon from '@mui/icons-material/Air';
+import WaterDamageRoundedIcon from '@mui/icons-material/WaterDamageRounded';
+import LandslideIcon from '@mui/icons-material/Landslide';
+import WaterIcon from '@mui/icons-material/Water';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
 
 const Homepage = () => {
-
   return (
+
     <div>
-        <Grid container
+      <br />
+      <br />
+      <br />
+      <Grid container
         direction="row"
         justifyContent="center"
         alignItems="center"
-        style={{
-            minHeight: "200vh"
-        }}
-        >
-            <Stack
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-            >
-                <img
-                alt='logo'
-                src={HelloFriend}
-                style={{
-                    width: '400px',
-                    height: '400px'
-                }}
-                />
+        spacing={1}
+        padding={2}
+      >
 
-                <Button variant='contained'
-                style={{
-                    width:"300px"
-                  }}
-                onClick={()=>{LogoutHydro()
-                  window.location.reload(true)
-                }}
+        {/*Humidity level*/}
+        <Grid item xs={12} sm={12} md={4} xl={4}>
+          <Paper elevation={0} sx={{
+            backgroundColor: "WHITE",
+            border: '2px solid #3B5437'
+          }}
+          >
+            <Grid
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              padding={2}
+            >
+              <Grid item xs={12} md={12}>
+                <Typography
+                  variant="h6"
+                  textAlign='center'
+                  color='#3C4048'>
+                  Humidity Level
+                </Typography>
+              </Grid>
+
+              <Grid item xs={12} md={12}>
+                <Stack
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}
                 >
-                Logout
-                </Button>
-            
-            </Stack>
-        
+                  <AirIcon fontSize="large" sx={{ color: '#3C4048' }}/>
+
+                  <Typography
+                  variant="h4"
+                  textAlign='center'
+                  color='#3C4048'>
+                    100
+                  </Typography>
+                </Stack>
+
+              </Grid>
+            </Grid>
+          </Paper>
         </Grid>
-  
+
+        {/*Water level*/}
+        <Grid item xs={12} sm={12} md={4} xl={4}>
+          <Paper elevation={0} sx={{
+            backgroundColor: "WHITE",
+            border: '2px solid #3B5437'
+          }}
+          > 
+            <Grid
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              padding={2}
+            >
+              <Grid item xs={12} md={12}>
+                <Typography
+                  variant="h6"
+                  textAlign='center'
+                  color='#47B5FF'>
+                  Water Level
+                </Typography>
+              </Grid>
+
+              <Grid item xs={12} md={12}>
+                <Stack
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}
+                >
+                  <WaterDamageRoundedIcon fontSize="large" sx={{ color: '#47B5FF' }}/>
+
+                  <Typography
+                  variant="h4"
+                  textAlign='center'
+                  color='#47B5FF'>
+                    100%
+                  </Typography>
+                </Stack>
+
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+
+       {/*TDS level*/}
+       <Grid item xs={12} sm={12} md={4} xl={4}>
+       <Paper elevation={0} sx={{
+         backgroundColor: "WHITE",
+         border: '2px solid #3B5437'
+       }}
+       > 
+         <Grid
+           container
+           direction="row"
+           justifyContent="center"
+           alignItems="center"
+           padding={2}
+         >
+           <Grid item xs={12} md={12}>
+             <Typography
+               variant="h6"
+               textAlign='center'
+               color='#665A48'>
+               TDS Level
+             </Typography>
+           </Grid>
+
+           <Grid item xs={12} md={12}>
+             <Stack
+             direction="row"
+             justifyContent="center"
+             alignItems="center"
+             spacing={2}
+             >
+                  <LandslideIcon fontSize="large" sx={{ color: '#665A48' }}/>
+
+                  <Typography
+                  variant="h4"
+                  textAlign='center'
+                  color='#665A48'>
+                    100
+                  </Typography>
+                </Stack>
+
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+
+        {/*Water pump*/}
+        <Grid item xs={12} sm={12} md={4} xl={4}>
+          <Paper elevation={0} sx={{
+            backgroundColor: "WHITE",
+            border: '2px solid #3B5437'
+          }}
+          > 
+            <Grid
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              padding={2}
+            >
+              <Grid item xs={12} md={12}>
+                <Typography
+                  variant="h6"
+                  textAlign='center'
+                  color='#0078AA'>
+                  Water pump
+                </Typography>
+              </Grid>
+
+              <Grid item xs={12} md={12}>
+                <Stack
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}
+                >
+                  <WaterIcon fontSize="large" sx={{ color: '#0078AA' }}/>
+                  
+                  <Switch
+                  checked={true}
+                  // onChange={handleChange}
+                  // inputProps={{ 'aria-label': 'controlled' }}
+                  style={{ color: '#0078AA' }}
+                />
+                </Stack>
+
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+
+        {/*Oxygen pump*/}
+        <Grid item xs={12} sm={12} md={4} xl={4}>
+          <Paper elevation={0} sx={{
+            backgroundColor: "WHITE",
+            border: '2px solid #3B5437'
+          }}
+          > 
+            <Grid
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              padding={2}
+            >
+              <Grid item xs={12} md={12}>
+                <Typography
+                  variant="h6"
+                  textAlign='center'
+                  color='#256D85'>
+                  Oxygen pump
+                </Typography>
+              </Grid>
+
+              <Grid item xs={12} md={12}>
+                <Stack
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}
+                >
+                  <AcUnitIcon fontSize="large" sx={{ color: '#256D85' }}/>
+
+                  <Switch
+                  checked={true}
+                  // onChange={handleChange}
+                  // inputProps={{ 'aria-label': 'controlled' }}
+                  style={{ color: '#256D85' }}
+                  />
+                </Stack>
+
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+
+      </Grid>
     </div>
   )
 }
