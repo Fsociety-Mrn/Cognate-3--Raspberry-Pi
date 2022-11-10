3188088abf6efa734a68b3cebb87a39f140abcf4
franz00007 <104089786+franz00007@users.noreply.github.com>
2022-11-10 14:07:07 +0800

    updated
    asd

--- main.py
+++ main.py
@@ -1,57 +1,67 @@
 import RPi.GPIO as GPIO
-# import time
+
 from Firebase.firebase import firebaseReadChild,firebaseUpdateChild
 
-# READ ME!
+import os
+import time
+import Adafruit_DHT
+import threading
 
-# update specific database value
-# 
-# firebaseUpdateChild(keyName,keyChild,value) with return true or false
-# 
-# Examples from database value:
-# LED 
-#  |_ turnON: false 
-# 
-# Python Code: 
-# keyName = "LED"
-# keyChild = "turnON"
-# value = True
-# print(firebaseUpdateChild(keyName,keyChild,value)) 
-# if success it will print "True"
+# Sensor should be set to Adafruit_DHT.DHT11,
+# Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
+sensor = Adafruit_DHT.DHT22
 
 
-# read specific database value
-# 
-# firebaseReadChild(keyName,valueName) return value function
-# 
-# Examples from database value:
-# LED 
-#  |_ turnON: true 
-# Python Code:
-# keyName = "LED"
-# keyChild = "turnON"
-# print(firebaseReadChild(keyName,valueName))
-# if success it will print the value from database 
 
-# setup functions
+# ********************** Pin Configuration ********************** #
+LED = 12
+Humid = 4
+floatSwitchh = 16
+
+# ********************** setup functions ********************** # 
 def setup():
-    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False) 
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(LED,GPIO.OUT) 
+    GPIO.setup(floatSwitchh,GPIO.OUT)
     
-    GPIO.setup(40,GPIO.OUT) #pinMode 40
-
-
-
-# loop function
-def loop():
+# ********************** functions ********************** #  
     
-    if (firebaseReadChild("LED","turnOn")):
-        GPIO.output(40,GPIO.HIGH)
+# for humidty function
+def Humidity():
+
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, Humid)
+
+    if humidity is not None and temperature is not None:
+        # print('Temp={0:1.0f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+        
+        firebaseUpdateChild("Humid","Humidity",str('Temp={0:1.0f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)).replace("Temp={0:1.0f}*C  Humidity=",""))
+
     else:
-        GPIO.output(40,GPIO.LOW)
+        print("wa;a")
+        #firebaseUpdateChild("Humid","Humidity","unable to read")
+
+    #time.sleep(5)
+        
+# Led function
+def Led():
+    if (firebaseReadChild("LED","turnOn")):
+        
+        GPIO.output(LED,GPIO.HIGH)
+    else:
+        GPIO.output(LED,GPIO.LOW)  
+    # print(firebaseReadChild("LED","turnOn"))
+
+
+# ********************** loop function ********************** #
+def loop():
+    # Led()
+
+    threading.Thread(target=Led, args=()).start()
+    threading.Thread(target=Humidity, args=()).start()
     
-    print(firebaseUpdateChild("LED","turnOn",False))
-    print(firebaseReadChild("LED","turnOn"))
-    
+
+
     return loop()
 
 
