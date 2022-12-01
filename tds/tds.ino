dd34a3cf7bcb17791e1228d3da196e6a520e2932
unknown <lisboamillen30@gmail.com>
2022-12-01 19:42:48 +0800

    updated

--- tds/tds.ino
+++ tds/tds.ino
@@ -5,6 +5,12 @@
 GravityTDS gravityTds;
  
 float temperature = 25,tdsValue = 0;
+
+//Sonar Sensor 1
+int Trig = 2;
+int Echo = 3;
+
+int Float = 4; //float switch 1
  
 void setup()
 {
@@ -13,15 +19,52 @@ void setup()
     gravityTds.setAref(5.0);  //reference voltage on ADC, default 5.0V on Arduino UNO
     gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
     gravityTds.begin();  //initialization
+    
+    pinMode(Trig, OUTPUT);
+    pinMode(Echo, INPUT);
+    
+    pinMode(Float, INPUT_PULLUP);
 }
  
 void loop()
 {
+  Serial.println(TDS() + " PPM," + String(tankLevel()));
+  delay(1000);
+}
+
+String TDS(){
     //temperature = readTemperature();  //add your temperature sensor and read it
     gravityTds.setTemperature(temperature);  // set the temperature and execute temperature compensation
     gravityTds.update();  //sample and calculate
     tdsValue = gravityTds.getTdsValue();  // then get the value
-    Serial.print(tdsValue,0);
-    Serial.println(" ppm");
+    //Serial.println(int(tdsValue));
+    //Serial.println(" ppm");
+   
+    return String(int(tdsValue));
     delay(1000);
 }
+
+// for tank level
+int tankLevel(){
+  
+  digitalWrite(Trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(Trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(Trig, LOW);
+
+//  duration
+  long duration = pulseIn(Echo, HIGH);
+
+//return an inches
+  int inches = duration / 74 / 2;
+
+  if (!digitalRead(Float)){
+    return inches;
+  }else{
+    return 100;
+  }
+    // return inches;
+}
+
+
