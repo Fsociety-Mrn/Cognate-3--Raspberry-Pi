b64d9a0c24256d22bda94ed3229ca35432b4756a
franz00007 <104089786+franz00007@users.noreply.github.com>
2022-12-21 16:47:39 +0800

    Update SerialCom.py

--- Arduino/SerialCom.py
+++ Arduino/SerialCom.py
@@ -1,17 +1,28 @@
 
 import serial
 import time
-
+import threading
 
 port = '/dev/ttyACM0'
 Arduino = serial.Serial(port ,9600, timeout=1)
 Arduino.reset_input_buffer()
 
-def readTDS():
-    Arduino.flush()
-    data = Arduino.readline().decode('utf-8').rstrip()
-    time.sleep(.5)
-    print(data)
-    return data
 
-# readTDS()
+def readTDS():
+    try:
+    
+        
+        Arduino.flush()
+        data = Arduino.readline().decode('utf-8').rstrip()
+        #print(data)
+        time.sleep(1)
+        return data
+    except:
+    
+        return "0 PPM"
+
+def A():
+    print(readTDS()[0])
+    
+def B():
+    print(readTDS()[1])
